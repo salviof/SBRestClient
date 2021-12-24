cc9f289525b1feae4cb87029f62ba407c328c17b
Salvio Furbino <salviof@gmail.com>
2021-12-24 00:15:12 -0300

    Atualizacao automática

--- src/main/java/com/super_bits/modulosSB/SBCore/integracao/libRestClient/api/token/TokenDeAcessoExternoDinamico.java
+++ src/main/java/com/super_bits/modulosSB/SBCore/integracao/libRestClient/api/token/TokenDeAcessoExternoDinamico.java
@@ -13,7 +13,7 @@ import java.util.Date;
  */
 public class TokenDeAcessoExternoDinamico extends TokenDeAcessoExternoSimples {
 
-    private Date dataHoraExpira;
+    private final Date dataHoraExpira;
 
     public TokenDeAcessoExternoDinamico(String pToken, Date pDataHoraExipira) {
         super(pToken);
